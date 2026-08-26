0e9a3f899e062ffb435ad3d18ea1f0b53b68998c
hariharasudhan.mg <hariharasudhanm046@gmail.com>
2026-08-26 11:03:12 +0530

    Change email transport service to SMTP host

--- Server/routes/emailService.js
+++ Server/routes/emailService.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   requireTLS: true,
